56b1b0ee6130af575528393d577511be951615c4
Yandaar SOMDA <ysomda@deezer.com>
2017-11-02 00:59:28 +0100

    Update setup script

--- setup.py
+++ setup.py
@@ -1,16 +1,50 @@
 #!/usr/bin/env python3
+"""
+* Install with pip (recommended):
+    pip3 install .
 
-from setuptools import setup, find_packages
+* Install with setuptools:
+    python setup.py install
+
+* Run tests:
+    python setup.py pytest
+
+* Build documentation:
+    pip install -r requirements-doc.txt
+    cd docs/
+    make clean html
+"""
+
+from pathlib import Path
+from setuptools import setup
 
 
 setup(
+    # Package info
     name="mod",
-    author="yoeo",
-    version="0.1",
-    url="https://github.deezerdev.com/ysomda/mod",
+    author="Y. SOMDA",
+    version='0.1',
+    url="https://github.com/yoeo/mod",
+    description="Modular arithmetic in Python",
+    long_description=Path(__file__).parent.joinpath(
+        'docs', 'index.rst').read_text(),
     license="MIT",
-    description="Mod arithmetics with integers in Python",
-    tests_require='pytest',
-    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    # Install setup
+    python_requires='>=3',
+    platforms='any',
+    py_modules=['mod'],
+    zip_safe=True,
+    # Test setup
     setup_requires=['pytest-runner'],
+    tests_require='pytest',
 )
